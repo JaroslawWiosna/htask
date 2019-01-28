0e82c2db254865008914e9ca80adf5b46922e7f1
Jarosław Wiosna <179892@edu.p.lodz.pl>
2019-01-28 17:14:38 +0100

    Print help

--- htask.hs
+++ htask.hs
@@ -11,6 +11,15 @@ listOfArgs = do
 	args <- getArgs
 	return (args !! 0)
 
+printHelp :: String
+printHelp = "htask help"
+
+initialBranchPrintHelp :: String -> IO ()
+initialBranchPrintHelp arg
+	| arg == "help" = putStrLn printHelp
+	| arg == "add"  = undefined
+	| otherwise     = undefined 
+
 main :: IO ()
 main = do
 	putStrLn "htask"
@@ -22,4 +31,6 @@ main = do
 	contents <- hGetContents handle
 	let singleWords = words contents
 	print singleWords
+	arg0 <- listOfArgs
+	initialBranchPrintHelp arg0	
 
